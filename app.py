76a900e2d8a1e1f907a83631ef872d84b758191e
Bugz <urigami2010@gmail.com>
2021-06-08 13:30:48 +0300

    getting colors from theme, and some modularity

--- app.py
+++ app.py
@@ -7,27 +7,25 @@ def main():
     # Settings
     st.set_page_config(layout="wide", page_title='Inshop Analytics tool')
     st.set_option('deprecation.showPyplotGlobalUse', False)
-    
+
     # Loading CSS
     cc.local_css("css/styles.css")
     cc.remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
-    
+
     st.sidebar.write('Sidebar to take space.')
-    
+    st.write(st.get_option('theme.primaryColor'))
+
     # Testing stNotification
-    with cc.stNotification("Counting till 5... without spinner",spinner=False):
+    with cc.stNotification("Counting till 5... without spinner", spinner=False):
         for i in range(5):
             st.write(i+1)
             time.sleep(1)
-            
+
     with cc.stNotification("Counting till 5... with a spinner"):
         for i in range(5):
             st.write(i+1)
             time.sleep(1)
-            
-            
-    
-    
+
 
 if __name__ == '__main__':
     main()
--- components.py
+++ components.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from string import Template
+
 
 def local_css(file_name):
     """
@@ -7,39 +9,49 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+
 def remote_css(url):
     """
     Injects css from the web
     """
-    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
+    st.markdown(f'<link href="{url}" rel="stylesheet">',
+                unsafe_allow_html=True)
 
 
 class stNotification:
-    def __init__(self,text,spinner=True):
+    def build_style(self):
+        "Getting theme colors"
+        pc = st.get_option('theme.primaryColor')
+        bc = st.get_option('theme.backgroundColor')
+        sbc = st.get_option('theme.secondaryBackgroundColor')
+        tc = st.get_option('theme.textColor')
+        return {'pc': pc, "bc": bc, "sbc": sbc, "tc": tc}
+
+    def __init__(self, text, spinner=True):
+        "Getting default theme and building style"
+        styles = self.build_style()
+
+        "Spinner"
         if spinner:
-            self.notification = f'''
-            <div class="custom-notification">
-                <div class="flex-container">
-                    <div>
-                        <div class="loader"></div>
-                    </div>
+            loader = f'<div ><div class = "loader" style ="line-height: 2rem;text-align: center;border-left: 0.3em solid {styles["pc"]};" ></div></div>'
+        else:
+            loader = '<br>'
+
+        "Building notification object"
+        self.notification = f'''
+            <div class="custom-notification" style="top:0rem;background-color: #0E1117;border-bottom-right-radius: 9px;border-bottom-left-radius: 9px;padding: 0.5rem;position: fixed;line-height: 2rem;text-align: center;border-left: 3px solid {styles['pc']};border-right: 3px solid {styles['pc']};border-bottom: 3px solid {styles['pc']}">
+                <div style="display: flex;flex-wrap: nowrap;">
+                    {loader}
                     <div style="margin-left:1rem;">
                         {text}
                     </div>
                 </div>
-            </div>'''
-        else:
-            self.notification = f'''
-            <div class="custom-notification">
-                <div class="flex-container">
-                    <div style="margin-left:1rem;margin-right:1rem;">
-                        {text}
-                    </div>
-                </div>
-            </div>'''
-            
+            </div>
+            '''
+
     def __enter__(self):
-        self.notification_object = st.markdown(self.notification, unsafe_allow_html=True) 
+        self.notification_object = st.markdown(
+            self.notification, unsafe_allow_html=True)
 
     def __exit__(self, *args, **kwargs):
         self.notification_object.empty()
