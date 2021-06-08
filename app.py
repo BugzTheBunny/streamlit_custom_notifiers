5300cd7d7eac637f8d115204ef55bb756ec65b78
Bugz <urigami2010@gmail.com>
2021-06-08 13:33:33 +0300

    changes

--- app.py
+++ app.py
@@ -13,13 +13,14 @@ def main():
     cc.remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
 
     st.sidebar.write('Sidebar to take space.')
-    st.write(st.get_option('theme.primaryColor'))
 
+    st.write('Test of counting without spinner')
     # Testing stNotification
     with cc.stNotification("Counting till 5... without spinner", spinner=False):
         for i in range(5):
             st.write(i+1)
             time.sleep(1)
+    st.write('Test of counting with spinner')
 
     with cc.stNotification("Counting till 5... with a spinner"):
         for i in range(5):
